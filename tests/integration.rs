9da64416029cc33908acf46aacbe423e3cc64131
Jeff Smits <jeff.smits@gmail.com>
2016-08-29 13:56:29 +0100

    Final fixes for windows integration tests
    Now using windows line endings in expected output when on the windows
    platform.

--- tests/integration.rs
+++ tests/integration.rs
@@ -13,6 +13,22 @@ macro_rules! new_scene {
 
 static BENCH_OUTPUT_1: &'static str = include_str!("fixtures/bench_output_1.txt");
 
+#[cfg(not(windows))]
+static SAME_INPUT: &'static str = include_str!("fixtures/same_input.expected");
+#[cfg(not(windows))]
+static DIFFERENT_INPUT: &'static str = include_str!("fixtures/different_input.expected");
+#[cfg(not(windows))]
+static DIFFERENT_INPUT_SELECTIONS: &'static str = include_str!("fixtures/different_input_selections\
+                                                                .expected");
+
+#[cfg(windows)]
+static SAME_INPUT: &'static str = include_str!("fixtures/same_input.windows.expected");
+#[cfg(windows)]
+static DIFFERENT_INPUT: &'static str = include_str!("fixtures/different_input.windows.expected");
+#[cfg(windows)]
+static DIFFERENT_INPUT_SELECTIONS: &'static str = include_str!("fixtures/different_input_selections\
+                                                                .windows.expected");
+
 fn new_ucmd() -> second_law::UCommand {
     let mut scene: second_law::Scene = new_scene!();
     scene.subcmd_arg("benchcmp");
@@ -39,22 +55,32 @@ fn help() {
 
 #[test]
 fn same_input() {
-    new_ucmd().args(&["bench_output_1.txt", "bench_output_1.txt"]).succeeds().stdout_is_fixture("same_input.expected");
+    new_ucmd().args(&["bench_output_1.txt", "bench_output_1.txt"]).succeeds().stdout_is(SAME_INPUT);
 }
 
 #[test]
 fn different_input() {
-    new_ucmd().args(&["bench_output_2.txt", "bench_output_3.txt"]).succeeds().stdout_is_fixture("different_input.expected");
+    new_ucmd()
+        .args(&["bench_output_2.txt", "bench_output_3.txt"])
+        .succeeds()
+        .stdout_is(DIFFERENT_INPUT);
 }
 
-// TODO: Add tests with inputs with non-overlapping names
+// TODO: Add tests with inputs with non-overlapping names and check stderr for the warnings
 
 #[test]
 fn different_input_selections() {
-    new_ucmd().args(&["dense::", "dense_boxed::", "bench_output_1.txt"]).succeeds().stdout_is_fixture("different_input_selections.expected");
+    new_ucmd()
+        .args(&["dense::", "dense_boxed::", "bench_output_1.txt"])
+        .succeeds()
+        .stdout_is(DIFFERENT_INPUT_SELECTIONS);
 }
 
 #[test]
 fn stdin() {
-    new_ucmd().args(&["dense::", "dense_boxed::", "-"]).pipe_in(BENCH_OUTPUT_1).succeeds().stdout_is_fixture("different_input_selections.expected");
+    new_ucmd()
+        .args(&["dense::", "dense_boxed::", "-"])
+        .pipe_in(BENCH_OUTPUT_1)
+        .succeeds()
+        .stdout_is(DIFFERENT_INPUT_SELECTIONS);
 }
