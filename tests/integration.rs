a31f3f1f0f554d7c7dae5bfa4df8989dc9090dff
Nick Fitzgerald <fitzgen@gmail.com>
2016-10-02 15:27:59 -0700

    Allow running as either `cargo benchcmp` or `cargo-benchcmp`
    Fixes #20.

--- tests/integration.rs
+++ tests/integration.rs
@@ -13,7 +13,6 @@ macro_rules! new_scene {
 
 fn new_ucmd() -> second_law::UCommand {
     let mut scene: second_law::Scene = new_scene!();
-    scene.subcmd_arg("benchcmp");
     scene.ucmd()
 }
 
@@ -55,7 +54,6 @@ fn non_overlapping_input() {
 #[test]
 fn different_input_colored() {
     let mut scene: second_law::Scene = new_scene!();
-    scene.subcmd_arg("benchcmp");
     // NOTE: keeping the environment here so that terminfo is available,
     //  which is required to get colour code in the output
     scene.ucmd_keepenv()
